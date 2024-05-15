e48150bebfbf953fa7392835c03d09a9ef5b0528
Maria <marycarol.00@gmail.com>
2024-05-15 20:45:56 -0300

    feat: requisito 6

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,48 @@
+import {
+  DataTypes,
+  Model,
+  InferCreationAttributes,
+  InferAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class User extends Model<InferAttributes<User>,
+InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
